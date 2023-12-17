cfa6ce81de077cf855bc2ef2c6ccd031901c9f46
Fedor Novikov <novikovfedor@gmail.com>
2023-12-17 20:20:46 +0200

    17 day fix

--- src/17-day-2.ts
+++ src/17-day-2.ts
@@ -180,14 +180,7 @@ function main(input: string) {
     }
 
     const pq = new BinaryHeap<[number, number, number, number, number, number]>({
-        comparator: (a, b) =>
-            a[0] !== b[0] ? b[0] - a[0]
-                : a[1] !== b[1] ? b[1] - a[1]
-                    : a[2] !== b[2] ? b[2] - a[2]
-                        : a[3] !== b[3] ? b[3] - a[3]
-                            : a[4] !== b[4] ? b[4] - a[4]
-                                : a[5] !== b[5] ? b[5] - a[5]
-                                    : 0
+        comparator: (a, b) => a[0] !== b[0] ? b[0] - a[0] : 0
     });
     pq.push([0, 0, 0, 0, 0, 0]);
 

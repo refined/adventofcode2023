7e7d45620b712fc2f5dae98296383408c6e9bd20
Fedor Novikov <novikovfedor@gmail.com>
2023-12-21 22:16:22 +0200

    21-2

--- src/21-day-2.ts
+++ src/21-day-2.ts
@@ -171,13 +171,13 @@ function getPointsFromZip(zip: string): coord[] {
 function zipPoints(points: coord[], input: string[]): string {
     let zip = input.join(";");
     for (const point of points) {
-        const i = point[0]*(input[0].length+1) + point[1];
-        zip = zip.substring(0,i) + "S" + zip.substring(i+1);
+        const i = point[0] * (input[0].length + 1) + point[1];
+        zip = zip.substring(0, i) + "S" + zip.substring(i + 1);
     }
     return zip;
 }
 
-function movePoints(field: OneField, rowMax: number, colMax: number, input: string[]): {newField: OneField, outs: OneField[]} {
+function movePoints(field: OneField, rowMax: number, colMax: number, input: string[]): { newField: OneField, outs: OneField[] } {
     const newPoints = [];
     const points = getPointsFromZip(field.zip);
     const outLeftPoints: coord[] = [];
@@ -186,22 +186,22 @@ function movePoints(field: OneField, rowMax: number, colMax: number, input: stri
     const outBottomPoints: coord[] = [];
     const outs: OneField[] = [];
 
-    for (const [i,j] of points) {
-        for (const dir of [[0,1],[0,-1],[1,0],[-1,0]]) {
+    for (const [i, j] of points) {
+        for (const dir of [[0, 1], [0, -1], [1, 0], [-1, 0]]) {
             const nextI = dir[0] + i;
             const nextJ = dir[1] + j;
-            const i1 = nextI >= 0 ? nextI % rowMax : rowMax + ((nextI+1) % rowMax) - 1;
-            const j1 = nextJ >= 0 ? nextJ % colMax : colMax + ((nextJ+1) % colMax) - 1;
+            const i1 = nextI >= 0 ? nextI % rowMax : rowMax + ((nextI + 1) % rowMax) - 1;
+            const j1 = nextJ >= 0 ? nextJ % colMax : colMax + ((nextJ + 1) % colMax) - 1;
 
             if (0 <= nextI && nextI < rowMax && 0 <= nextJ && nextJ < colMax) {
                 if (input[nextI][nextJ] !== "#") {
                     newPoints.push([nextI, nextJ])
                 }
             } else {
-                if (nextI < 0) outLeftPoints.push([i1, j1]);
-                if (nextJ < 0) outTopPoints.push([i1, j1]);
-                if (nextI >= rowMax) outRightPoints.push([i1, j1]);
-                if (nextJ >= colMax) outBottomPoints.push([i1, j1]);
+                if (nextI < 0) outTopPoints.push([i1, j1]);
+                if (nextJ < 0) outLeftPoints.push([i1, j1]);
+                if (nextI >= rowMax) outBottomPoints.push([i1, j1]);
+                if (nextJ >= colMax) outRightPoints.push([i1, j1]);
             }
         }
     }
@@ -231,9 +231,13 @@ function movePoints(field: OneField, rowMax: number, colMax: number, input: stri
     }
 
     return {
-        newField: {coords: field.coords, zip: zipPoints(newPoints, input)},
+        newField: { coords: field.coords, zip: zipPoints(newPoints, input) },
         outs: outs
     }
+}
+
+function sumOnField(zip: string): number {
+    return [...zip].filter(ch => ch === "S").length;
 }
 
 function main(input: string, steps: number) {
@@ -255,25 +259,48 @@ function main(input: string, steps: number) {
             baseField.push(line);
         }
     }
-    const rowMax = lines.length;
-    const colMax = lines[0].length;
+    const rowMax = lines.length; // 131
+    const colMax = lines[0].length; // 131
 
     const fields = new Map<string, string>(); // [i,j] -> string
-    const cache = new Map<string, {newField: OneField, outs: OneField[]}>(); // string -> string
+    const cache = new Map<string, { newField: OneField, outs: OneField[] }>(); // string -> string
     fields.set("0,0", lines.join(";"));
-    
-    for (let _ = 0; _ < steps; _++) {
+
+    for (let counter = 0; counter < steps; counter++) {
+        if (counter === 65 + 131 * 3) {
+            const iterates = (steps - 65) / 131;
+            const odd = Math.pow((iterates), 2);
+            const even = Math.pow((iterates - 1), 2);
+            sum = sumOnField(fields.get("0,131")) * even + sumOnField(fields.get("0,262")) * odd // all full square
+                + sumOnField(fields.get("0,393")) // top, left, right, bottom
+                + sumOnField(fields.get("0,-393"))
+                + sumOnField(fields.get("-393,0"))
+                + sumOnField(fields.get("393,0"))
+
+                + sumOnField(fields.get("262,131")) * (iterates - 1) // not very full square on edges
+                + sumOnField(fields.get("-262,131")) * (iterates - 1)
+                + sumOnField(fields.get("262,-131")) * (iterates - 1)
+                + sumOnField(fields.get("-262,-131")) * (iterates - 1)
+
+                + sumOnField(fields.get("393,131")) * (iterates) // small square on edges
+                + sumOnField(fields.get("-393,131")) * (iterates)
+                + sumOnField(fields.get("393,-131")) * (iterates)
+                + sumOnField(fields.get("-393,-131")) * (iterates)
+                ;
+            break;
+        }
+
         const newFields = [];
         for (const [coords, field] of fields.entries()) {
             let p = cache.get(field);
             const cor: coord = coords.split(",").map(Number) as coord;
             if (!p) {
-                p = movePoints({coords: cor, zip: field}, rowMax, colMax, baseField);
+                p = movePoints({ coords: cor, zip: field }, rowMax, colMax, baseField);
                 cache.set(field, p);
             }
             fields.set(coords, p.newField.zip);
             for (const o of p.outs) {
-                const newCoords = [cor[0]+o.coords[0],cor[1]+o.coords[1]];
+                const newCoords = [cor[0] + o.coords[0], cor[1] + o.coords[1]];
                 if (!fields.has(newCoords.join(","))) {
                     newFields.push([newCoords.join(","), o.zip])
                 };
@@ -284,21 +311,17 @@ function main(input: string, steps: number) {
         }
         // console.log("fields: " + fields.size);
 
-        if (_%1000 === 0 && _ > 0) {
+        if (counter % 1000 === 0 && counter > 0) {
             sum = 0;
             [...fields.values()].forEach(v => sum += [...v].filter(ch => ch === "S").length);
-            console.log("i:" + _ + " sum:" + sum + " cachesize:" + cache.size);
+            console.log("i:" + counter + " sum:" + sum + " cachesize:" + cache.size);
         }
     }
 
-    sum = 0;
-    [...fields.values()].forEach(v => sum += [...v].filter(ch => ch === "S").length);
     console.log(sum);
 }
 
 console.log("test");
-main(test, 50);
-main(test, 5000);
 console.log("task");
 main(task, 26501365);
 console.log("done");
